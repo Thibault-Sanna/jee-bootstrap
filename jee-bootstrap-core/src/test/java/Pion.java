c91f20bb269deb3765737ab8c273d19457f06198
BertrandVa <bertrand.valentin@isen-lille.fr>
2017-01-09 16:00:01 +0100

    envoi de la classe Pion

---
+++ jee-bootstrap-core/src/test/java/Pion.java
@@ -0,0 +1,21 @@
+
+public class Pion {
+	
+	private Boolean isDame;
+	private String Colour;
+	//private position;  à implémenter en fonction du tableau
+	
+	public Boolean getIsDame() {
+		return isDame;
+	}
+	public void setIsDame(Boolean isDame) {
+		this.isDame = isDame;
+	}
+	public String getColour() {
+		return Colour;
+	}
+	public void setColour(String colour) {
+		Colour = colour;
+	}
+	
+}
